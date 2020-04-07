53b3c340c8eb6acdaab7b9049d584f14b4eab32a
Omar Abbasi <oabbasi980@gmail.com>
2020-04-07 17:05:03 -0400

    Friend Finder HW 11

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+      name: "Ahmed",
+      photo: "https://pmcvariety.files.wordpress.com/2018/11/rocky.jpg?w=600",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Jacob Deming",
+      photo: "https://pmcvariety.files.wordpress.com/2018/11/rocky.jpg?w=600",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Jeremiah Scanlon",
+      photo: "https://pmcvariety.files.wordpress.com/2018/11/rocky.jpg?w=600",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Louis T. Delia",
+      photo: "https://pmcvariety.files.wordpress.com/2018/11/rocky.jpg?w=600",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Lou Ritter",
+      photo: "https://cdn.vox-cdn.com/thumbor/BG8y02N7e3b83gIDZxC4jMC7--o=/0x0:1024x683/920x613/filters:focal(0x0:1024x683):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47776447/rambo.0.0.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Jordan Biason",
+      photo: "https://cdn.vox-cdn.com/thumbor/BG8y02N7e3b83gIDZxC4jMC7--o=/0x0:1024x683/920x613/filters:focal(0x0:1024x683):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47776447/rambo.0.0.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  ];
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require("express")
 var app = express()
 
-var PORT = proces.env.PORT || 8080
+var PORT = process.env.PORT || 8080
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-require("./app/routing/apiroutes")
-require("./app/routing/htmlroutes")
+require("./app/routing/apiroutes")(app)
+require("./app/routing/htmlroutes")(app)
 app.listen(PORT, function(){
     console.log("app listening on PORT: " + PORT)
 })
